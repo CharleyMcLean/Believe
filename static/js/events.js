aff8667014b4fc3a7b1a67a155fb46c65b797a67
CharleyMcLean <charleyaustinmclean@gmail.com>
2016-11-12 01:23:21 +0000

    Have toggling buttons for population layer.  Need to change default gradient for population layer - have only changed the toggle gradient (which actually needs to be the normal default gradient so it will be oppositie of the UFO layer).

--- static/js/events.js
+++ static/js/events.js
@@ -1,152 +1,100 @@
 $(document).ready(function () {
   
-  var map, heatmap;
+    var map, heatmap, popHeatmap;
 
-  function initMap() {
+    function initMap() {
 
+        // Center the map on the US.
+        var myLatLng = {lat: 39.5, lng: -98.35};
 
-    // Center the map on the US.
-    var myLatLng = {lat: 39.5, lng: -98.35};
+        // Create a map object and specify the DOM element for display.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          zoom: 3,
+          zoomControl: true,
+          styles: MAPSTYLES,
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        }); //end new map
+        console.log("map exists");
+        getPoints();
+        getPopPoints();
+    } //end initMap
 
-    // Create a map object and specify the DOM element for display.
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: myLatLng,
-      zoom: 3,
-      zoomControl: true,
-      styles: MAPSTYLES,
-      mapTypeId: google.maps.MapTypeId.TERRAIN
-    }); //end new map
-    console.log("map exists");
-    getPoints();
-    getPopPoints();
-  } //end initMap
+///////////////////////////////////////////////////////////////////////////
 
+    function toggleHeatmap() {
+        heatmap.setMap(heatmap.getMap() ? null : map);
+    } //end toggleHeatmap
 
-  // --------------------------------------------------------------//
-  // --------------------------------------------------------------//
-  // If you want to create a StyledMapType to make a map type control
-  // create it like this:
-
-  // Create a new StyledMapType object, passing it the array of styles,
-  // as well as the name to be displayed on the map type control.
-  // var styledMap = new google.maps.StyledMapType(
-  //     MAPSTYLES,
-  //     {name: "Custom Style"}
-  // );
-  // You would then set 'styles' in the mapoptions to 'styledMap'
-
-  // Associate the styled map with the MapTypeId and set it to display.
-  // map.mapTypes.set('map_style', styledMap);
-  // map.setMapTypeId('map_style');
-  // --------------------------------------------------------------//
-  // --------------------------------------------------------------//
-
-  function toggleHeatmap() {
-    heatmap.setMap(heatmap.getMap() ? null : map);
-  }
-
-  function changeGradient() {
-    var gradient = [
-      'rgba(0, 255, 255, 0)',
-      'rgba(0, 255, 255, 1)',
-      'rgba(0, 191, 255, 1)',
-      'rgba(0, 127, 255, 1)',
-      'rgba(0, 63, 255, 1)',
-      'rgba(0, 0, 255, 1)',
-      'rgba(0, 0, 223, 1)',
-      'rgba(0, 0, 191, 1)',
-      'rgba(0, 0, 159, 1)',
-      'rgba(0, 0, 127, 1)',
-      'rgba(63, 0, 91, 1)',
-      'rgba(127, 0, 63, 1)',
-      'rgba(191, 0, 31, 1)',
-      'rgba(255, 0, 0, 1)'
-    ]; //end var gradient
-      heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+    function changeGradient() {
+        var gradient = [
+            'rgba(0, 255, 255, 0)',
+            'rgba(0, 255, 255, 1)',
+            'rgba(0, 191, 255, 1)',
+            'rgba(0, 127, 255, 1)',
+            'rgba(0, 63, 255, 1)',
+            'rgba(0, 0, 255, 1)',
+            'rgba(0, 0, 223, 1)',
+            'rgba(0, 0, 191, 1)',
+            'rgba(0, 0, 159, 1)',
+            'rgba(0, 0, 127, 1)',
+            'rgba(63, 0, 91, 1)',
+            'rgba(127, 0, 63, 1)',
+            'rgba(191, 0, 31, 1)',
+            'rgba(255, 0, 0, 1)'
+        ]; //end var gradient
+        
+        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
     } //end changeGradient
 
-  function changeRadius() {
-    heatmap.set('radius', heatmap.get('radius') ? null : 20);
-  } //end changeRadius
+    function changeRadius() {
+        heatmap.set('radius', heatmap.get('radius') ? null : 20);
+    } //end changeRadius
 
-  function changeOpacity() {
-    heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-  } //end changeOpacity
+    function changeOpacity() {
+        heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+    } //end changeOpacity
 
   
-  function getPoints() {
+    function getPoints() {
     // Retrieving the information with AJAX
-    $.get('/events.json', function (events) {
-        // Create an array of lat/long points from returned JSON
-        
-        // Create an empty array to hold the map point data
-        var heatMapData = [];
-  
-        // Defined a function to create a new map point from each report 
-        // This is then pushed to the JS array we created.
-        
-        for (var key in events) {
-          var report = events[key];
-          // console.log(report);
-          if (!(report.latitude == 48 & report.longitude == -122)) {
-            heatMapData.push(new google.maps.LatLng(report.latitude, report.longitude));
-          } //end if statement
-          else {
-            console.log("skipped");
-          } //end else statement
-          console.log(report.latitude);
-          console.log(report.longitude);
-        } //end for loop
+        $.get('/events.json', function (events) {
+            // Create an array of lat/long points from returned JSON
+            
+            // Create an empty array to hold the map point data
+            var heatMapData = [];
+      
+            // Defined a function to create a new map point from each report 
+            // This is then pushed to the JS array we created.
+            
+            for (var key in events) {
+                var report = events[key];
+                // console.log(report);
+                if (!(report.latitude == 48 & report.longitude == -122)) {
+                heatMapData.push(new google.maps.LatLng(report.latitude, report.longitude));
+                } //end if statement
+                else {
+                console.log("skipped");
+                } //end else statement
+                console.log(report.latitude);
+                console.log(report.longitude);
+            } //end for loop
 
-        console.log(heatMapData);
-        console.log(typeof(heatMapData));
-        console.log($.isArray(heatMapData));
-        // return heatMapData;
+            console.log(heatMapData);
+            console.log(typeof(heatMapData));
+            console.log($.isArray(heatMapData));
+            // return heatMapData;
 
-        console.log("about to add heatmap");
-        heatmap = new google.maps.visualization.HeatmapLayer({
-          data: heatMapData,
-          map: map,
-          radius: 20
-        }); //end new heatmap
-        console.log("just added heatmap");
-      }); //end $.get
-  } //end getPoints()
+            console.log("about to add heatmap");
+            heatmap = new google.maps.visualization.HeatmapLayer({
+                data: heatMapData,
+                map: map,
+                radius: 20
+            }); //end new heatmap
+            console.log("just added heatmap");
+        }); //end $.get
+    } //end getPoints()
 
-
-  function getPopPoints() {
-    // Retrieving the information with AJAX
-    $.get('/population.json', function (city_pops) {
-        // Create an array of lat/long points from returned JSON
-        
-        // Create an empty array to hold the map point data
-        var heatMapPopData = [];
-  
-        // Defined a function to create a new map point from each report 
-        // This is then pushed to the JS array we created.
-        
-        for (var key in city_pops) {
-            var city = city_pops[key];
-            // console.log(report);
-            heatMapPopData.push({location: new google.maps.LatLng(city.latitude, city.longitude), weight: city.population});
-            console.log(city.latitude);
-            console.log(city.longitude);
-        } //end for loop
-
-        // console.log(heatMapPopData);
-        // console.log(typeof(heatMapPopData));
-        // console.log($.isArray(heatMapPopData));
-        // return heatMapPopData;
-
-        console.log("about to add pop heatmap");
-        popHeatmap = new google.maps.visualization.HeatmapLayer({
-          data: heatMapPopData,
-          map: map,
-          radius: 20
-        }); //end new heatmap
-        console.log("just added pop heatmap");
-      }); //end $.get
-  } //end getPoints()
 
     $("#toggle-heatmap").click(function() {
         toggleHeatmap();
@@ -159,6 +107,92 @@ $(document).ready(function () {
     });
     $("#change-opacity").click(function() {
         changeOpacity();
+    });
+
+
+///////////////////////////////////////////////////////////////////////////
+    function togglePopHeatmap() {
+        popHeatmap.setMap(popHeatmap.getMap() ? null : map);
+    } //end togglePopHeatmap
+
+    // This is not changing the default gradient, only changing what it changes
+    // to on toggle.  Need to figure out how to change the default gradient.
+    function changePopGradient() {
+        var popGradient = [
+            'rgba(180, 100, 50, 0)',
+            'rgba(180, 100, 50, 1)',
+            'rgba(195, 100, 50, 1)',
+            'rgba(210, 100, 50, 1)',
+            'rgba(225, 100, 50, 1)',
+            'rgba(240, 100, 50, 1)',
+            'rgba(240, 100, 44, 1)',
+            'rgba(240, 100, 37, 1)',
+            'rgba(240, 100, 31, 1)',
+            'rgba(240, 100, 25, 1)',
+            'rgba(282, 100, 18, 1)',
+            'rgba(330, 100, 25, 1)',
+            'rgba(350, 100, 37, 1)',
+            'rgba(0, 100, 50, 1)'
+        ]; //end var popGradient
+        
+        popHeatmap.set('gradient', popHeatmap.get('gradient') ? null : popGradient);
+    } //end changePopGradient
+
+    function changePopRadius() {
+        popHeatmap.set('radius', popHeatmap.get('radius') ? null : 20);
+    } //end changePopRadius
+
+    function changePopOpacity() {
+        popHeatmap.set('opacity', popHeatmap.get('opacity') ? null : 0.2);
+    } //end changePopOpacity
+
+
+
+    function getPopPoints() {
+        // Retrieving the information with AJAX
+        $.get('/population.json', function (city_pops) {
+            // Create an array of lat/long points from returned JSON
+            
+            // Create an empty array to hold the map point data
+            var heatMapPopData = [];
+      
+            // Defined a function to create a new map point from each report 
+            // This is then pushed to the JS array we created.
+            
+            for (var key in city_pops) {
+                var city = city_pops[key];
+                // console.log(report);
+                heatMapPopData.push({location: new google.maps.LatLng(city.latitude, city.longitude), weight: city.population});
+                console.log(city.latitude);
+                console.log(city.longitude);
+            } //end for loop
+
+            // console.log(heatMapPopData);
+            // console.log(typeof(heatMapPopData));
+            // console.log($.isArray(heatMapPopData));
+            // return heatMapPopData;
+
+            console.log("about to add pop heatmap");
+            popHeatmap = new google.maps.visualization.HeatmapLayer({
+              data: heatMapPopData,
+              map: map,
+              radius: 20
+            }); //end new heatmap
+            console.log("just added pop heatmap");
+        }); //end $.get
+    } //end getPoints()
+
+    $("#toggle-pop-heatmap").click(function() {
+        togglePopHeatmap();
+    });
+    $("#change-pop-gradient").click(function() {
+        changePopGradient();
+    });
+    $("#change-pop-radius").click(function() {
+        changePopRadius();
+    });
+    $("#change-pop-opacity").click(function() {
+        changePopOpacity();
     });
 
 
