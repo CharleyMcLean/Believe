f14272f02b1a2d54c7209d782464acd8aedd17cc
CharleyMcLean <charleyaustinmclean@gmail.com>
2016-12-06 06:27:59 +0000

    Fixed PORT for deployment

--- server.py
+++ server.py
@@ -274,4 +274,6 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-    app.run(debug=True, host="0.0.0.0")
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=PORT)
